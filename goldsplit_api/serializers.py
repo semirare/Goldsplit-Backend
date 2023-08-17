e3636e8fada8f7d0e0a72f26d816afa81ec749fa
dalton.harenza <dalton.harenza@atlanticventurepartners.tech>
2023-08-17 14:05:26 -0700

    update syntax

--- goldsplit_api/serializers.py
+++ goldsplit_api/serializers.py
@@ -1,17 +1,17 @@
-from rest_framework import serializers
+from rest_framework.serializers import ModelSerializer
 from .models import Runs, Splits, Games
 
-class GamesSerializer(serializers.ModelSerializer):
+class GamesSerializer(ModelSerializer):
     class Meta:
         model = Games
         fields = ['name', 'release_year']
 
-class RunsSerializer(serializers.ModelSerializer):
+class RunsSerializer(ModelSerializer):
     class Meta:
         model = Runs
         fields = ['id', 'game', 'category_name']
 
-class SplitsSerializer(serializers.ModelSerializer):
+class SplitsSerializer(ModelSerializer):
     class Meta:
         model = Splits
         fields = ['name', 'run', 'time', 'total_time', 'gold_time', 'gold_total_time', 'average_time']
