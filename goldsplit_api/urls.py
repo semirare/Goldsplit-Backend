249fbaa0b4cf3afe62048ddc3f5409bd63a22beb
dalton.harenza <dalton.harenza@atlanticventurepartners.tech>
2023-08-18 11:33:17 -0700

    use uuid in run details route

--- goldsplit_api/urls.py
+++ goldsplit_api/urls.py
@@ -11,6 +11,6 @@ router.register('games', GamesViewSet)
 urlpatterns = [
     path('', include(router.urls)),
     path('upload/', SplitsUploadView.as_view()),
-    path('run/details/<str:run_id>/', RunDetailsView.as_view()),
+    path('run/details/<uuid:run_id>/', RunDetailsView.as_view()),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
