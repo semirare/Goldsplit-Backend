4b1edd4a05b54519e3228044f3f2cb68e11b933c
dalton.harenza <dalton.harenza@atlanticventurepartners.tech>
2023-08-18 09:52:47 -0700

    add run details endpoint

--- goldsplit_api/views.py
+++ goldsplit_api/views.py
@@ -3,6 +3,7 @@ from django.db import transaction
 from rest_framework import viewsets, permissions
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.exceptions import ParseError, ValidationError
 
 from .serializers import *
 from .models import Runs, Splits, Games
@@ -15,6 +16,30 @@ class GamesViewSet(viewsets.ModelViewSet):
 class RunsViewSet(viewsets.ModelViewSet):
     queryset = Runs.objects.all()
     serializer_class = RunsSerializer
+
+class RunDetailsView(APIView):
+
+    def get(self, request, run_id):
+        if not run_id:
+            raise ParseError()
+        
+        try:
+            run = Runs.objects.get(id=run_id)
+        except:
+            raise ValidationError
+
+        splits = run.splits_set.all().order_by('total_time')
+
+        split_serializer = SplitsSerializer(splits, many=True)
+        
+        run_data = {
+            'game_name': run.game.name,
+            'category_name': run.category_name,
+            'total_time': splits[len(splits)-1].total_time,
+            'splits': split_serializer.data
+        }
+
+        return Response(data=run_data, status=200)
     
 class SplitsViewSet(viewsets.ModelViewSet):
     queryset = Splits.objects.all()
@@ -29,10 +54,8 @@ class SplitsUploadView(APIView):
 
         #check if games is already in db, if not then create it
         game = Games.objects.filter(name=splits['game_name'])
-        print(game)
         if game.exists():
             game = game[0]
-            print(game)
         else:        
             game = Games.objects.create_game(name=splits['game_name'])   
 
